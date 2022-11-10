4e3e94c8f71b9a685ca3e293d0b98c3572cb1775
«Wakeria» <MatrosovaYu@gmail.com>
2022-11-10 13:47:45 +0300

    Task24

---
+++ taskk/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+
+Console.WriteLine("введите свой вораст: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int square = number + 1;
+
+Console.WriteLine($"В следующем году вам исполнится {square}");
