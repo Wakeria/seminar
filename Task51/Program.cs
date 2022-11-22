17207ce4ea645a816b35071688336b08517c6198
«Wakeria» <MatrosovaYu@gmail.com>
2022-11-23 01:07:19 +0300

    Task 51

---
+++ Task51/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+
+int[,] CreatematrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void Printmatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int SumDiagonal(int[,] matrix)
+{
+    int sum = default;
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+        {
+            if (i == j)
+            {
+                sum = matrix[i, j] + sum;
+            }
+        }
+    }
+    return sum;
+}
+
+int[,] mtr = CreatematrixRndInt(3, 5, -10, 10);
+Printmatrix(mtr);
+int result = SumDiagonal(mtr);
+Console.WriteLine(result);
